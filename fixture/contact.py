4297e6ba2ff7c9d92f4ff7e4b787bba31c53fb8c
Irina <irina1980311@mail.ru>
2017-09-24 18:05:49 -0400

    Сделан тест удаления контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -30,3 +30,10 @@ class ContactHelper:
         wd.find_element_by_name("email").send_keys(contact.email)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        #wd.find_element_by_name("DeleteSel()").click()
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
