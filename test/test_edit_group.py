0db077449ada5d144b88e165d0b5b05fa319498a
Irina <irina1980311@mail.ru>
2017-09-30 18:44:24 -0400

    Выполнено предусловие для теста модификации группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,14 +2,19 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     # app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="New group"))
     # app.session.logout()
 
 
+
 def test_edit_group_header(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="New header"))
+    if app.group.count() == 0:
+        app.group.create(Group(header="test"))
+    #app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="New header"))
     # app.session.logout()
 
 
