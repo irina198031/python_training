a709c751d3cd86cc7dc758d6cba0007c2c9333cf
Irina <irina1980311@mail.ru>
2017-09-30 18:19:21 -0400

    Выполнено предусловие в тесте модификации контакта

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,8 +1,11 @@
-
+from model.contact import Contact
 
 def test_edit_first_contact(app):
 
-    app.open_home_page()
+    if app.contact.count() == 0:
+        app.contact.create(Contact(fname="first", lname="last", homeaddress="address", phone="789456123", email="first.last@gmail.com"))
+
+    #app.open_home_page()
 
     app.contact.edit_first_contact()
     # app.session.logout()
