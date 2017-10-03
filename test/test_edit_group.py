c1aff2c20b85eb8b7e7a52478bfa0cdb099359ae
Irina <irina1980311@mail.ru>
2017-10-02 21:27:35 -0400

    Загружаем информацию из тестируемого приложения в виде списков

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,8 +4,11 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     # app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
     # app.session.logout()
 
 
@@ -13,8 +16,11 @@ def test_edit_group_name(app):
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="test"))
+    old_groups = app.group.get_group_list()
     #app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
     # app.session.logout()
 
 
