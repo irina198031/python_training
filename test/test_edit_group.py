e841051ca8d0f7038ba51f67dbf8ffac60068d22
Irina <irina1980311@mail.ru>
2017-10-04 10:20:14 -0400

    Сравнение списков групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,23 +5,26 @@ def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
+    group = Group(name="New group")
+    group.id = old_groups[0].id
     # app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="New group"))
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0]=group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     # app.session.logout()
 
 
 
-def test_edit_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(header="test"))
-    old_groups = app.group.get_group_list()
-    #app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(header="New header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-    # app.session.logout()
+#def test_edit_group_header(app):
+    #if app.group.count() == 0:
+        #app.group.create(Group(header="test"))
+    #old_groups = app.group.get_group_list()
+    #app.group.edit_first_group(Group(header="New header"))
+    #new_groups = app.group.get_group_list()
+    #assert len(old_groups) == len(new_groups)
+
 
 
 
