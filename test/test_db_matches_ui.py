df53c4fdee87d95cb72183156f1d9aaf04f597ce
Irina <irina1980311@mail.ru>
2017-11-15 16:16:43 -0500

    изменен тест

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -14,6 +14,6 @@ def test_group_list(app, db):
 def test_contact_list(app, db):
     ui_list = app.contact.get_contact_list()
     def clean(contact):
-        return Contact(id=contact.id, name=contact.name.strip())
+        return Contact(id=contact.id, name=contact.firstname.strip())
     db_list = db.get_contact_list()
     assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
